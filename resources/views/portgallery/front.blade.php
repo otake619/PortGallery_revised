503fc2828a00db2452fe5e9e25a05855bea95a3d
otake619 <ryoheiotake@gmail.com>
2020-09-29 17:07:46 +0900

    bladeにトップページを追加。

---
+++ resources/views/portgallery/front.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/app.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PortGallery</title>
+</head>
+<body>
+    <div class="container-fluid pl-0 pr-0">
+        <div class="row no-gutters">
+            <div class="col-12">
+                <div class="header">
+                    <!--Navbar-->
+                    <nav class="navbar navbar-light purple lighten-4 mb-4">
+
+                        <!-- Navbar brand -->
+                        <a class="navbar-brand" href="#">Navbar</a>
+                    
+                        <!-- Collapse button -->
+                        <button class="navbar-toggler toggler-example purple darken-3" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent41" aria-controls="navbarSupportedContent41" aria-expanded="false"
+                        aria-label="Toggle navigation"><span class="white-text"><i class="fas fa-bars fa-1x"></i></span></button>
+                    
+                        <!-- Collapsible content -->
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent41">
+                    
+                        <!-- Links -->
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Pricing</a>
+                                </li>
+                            </ul>
+                        <!-- Links -->
+                    
+                        </div>
+                        <!-- Collapsible content -->
+                    
+                    </nav>
+                    <!--/.Navbar-->
+                    <div class="header-link">
+                        <a href="">お知らせ</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="./js/app.js"></script>
+</body>
+</html>
